8887b175299a5870c53f83cb455286427b01ce64
huangwenlong <1296979998@qq.com>
2018-07-18 22:24:02 +0800

    build new module spring-boot-learn and add HelloSpringBoot

---
+++ spring-boot-learn/src/main/java/org/xfl/springbootlearn/SpringBootLearnApplication.java
@@ -0,0 +1,12 @@
+package org.xfl.springbootlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootLearnApplication.class, args);
+    }
+}
