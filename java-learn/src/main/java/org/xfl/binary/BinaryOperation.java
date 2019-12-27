35220a0a1d557e59d15fb99c291da408f54aec1c
xfl0927 <xfl0927@126.com>
2019-12-27 18:09:55 +0800

    bit operation

---
+++ java-learn/src/main/java/org/xfl/binary/BinaryOperation.java
@@ -0,0 +1,46 @@
+package org.xfl.binary;
+
+public class BinaryOperation {
+    public static void main(String[] args){
+        //yuOperation();
+        leftOperation();
+        rightOperation();
+        unsignedRightOperation();
+
+    }
+
+    // <p> & </p>
+    public static void yuOperation(){
+        System.out.println("2 binary value:" + Integer.toBinaryString(2));
+        System.out.println("3 binary value:" + Integer.toBinaryString(3));
+        int res = 2&3;
+        System.out.println("2&3 binary value:" +Integer.toBinaryString(res));
+        System.out.println("2&3 int value:" + res);
+    }
+    // <p> << </p>
+    public static void leftOperation(){
+        System.out.println("20 binary value:" + Integer.toBinaryString(20));
+        int res = 20 << 8;
+        System.out.println("20 << 8 binary value:" +Integer.toBinaryString(res));
+        System.out.println("20 << 8 int value:" + res);
+        System.out.println("20 * 2的8次方:" + 20 * (int)Math.pow(2,8));
+    }
+    // <p> >> </p>
+    public static void rightOperation(){
+        System.out.println("613 binary value:" + Integer.toBinaryString(613));
+        //int res = -613 >> 8;
+        int res = 613 >> 8;
+        System.out.println("613 >> 8 binary value:" +Integer.toBinaryString(res));
+        System.out.println("613 >> 8 int value:" + res);
+        System.out.println("613 / 2的8次方 余数:" + (613/(int) Math.pow(2,8)));
+
+    }
+    // <p> >>> </p>
+    public static void unsignedRightOperation(){
+        System.out.println("-613 binary value:" + Integer.toBinaryString(-613));
+        int res = -613 >>> 8;
+        //int res = 613 >>> 8;
+        System.out.println("-613 >>> 8 binary value:" +Integer.toBinaryString(res));
+        System.out.println("-613 >>> 8 int value:" + res);
+    }
+}
