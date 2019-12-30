a28c9ba2e877d9667b1d15e0a21d55f12c828dbe
xfl0927 <xfl0927@126.com>
2019-12-30 13:39:01 +0800

    complete bit operation

--- java-learn/src/main/java/org/xfl/binary/BinaryOperation.java
+++ java-learn/src/main/java/org/xfl/binary/BinaryOperation.java
@@ -2,20 +2,46 @@ package org.xfl.binary;
 
 public class BinaryOperation {
     public static void main(String[] args){
-        //yuOperation();
-        leftOperation();
-        rightOperation();
-        unsignedRightOperation();
+        yuOperation();
+        huoOperation();
+        fanOperation();
+        yiHuoOperation();
+        //leftOperation();
+        //rightOperation();
+        //unsignedRightOperation();
 
     }
 
-    // <p> & </p>
+    // <p> & </p> 两位同为1 才为1
     public static void yuOperation(){
         System.out.println("2 binary value:" + Integer.toBinaryString(2));
         System.out.println("3 binary value:" + Integer.toBinaryString(3));
         int res = 2&3;
         System.out.println("2&3 binary value:" +Integer.toBinaryString(res));
         System.out.println("2&3 int value:" + res);
+    }
+    // <p> | </p> 只要有1 则为1
+    public static void huoOperation(){
+        System.out.println("2 binary value:" + Integer.toBinaryString(2));
+        System.out.println("3 binary value:" + Integer.toBinaryString(3));
+        int res = 2 | 3;
+        System.out.println("2|3 binary value:" +Integer.toBinaryString(res));
+        System.out.println("2|3 int value:" + res);
+    }
+    // <p> ~ </p> 取反 1->0  0->1
+    public static void fanOperation(){
+        System.out.println("3 binary value:" + Integer.toBinaryString(3));
+        int res =  ~3;
+        System.out.println("~3 binary value:" +Integer.toBinaryString(res));
+        System.out.println("~3 int value:" + res);
+    }
+    // <p> ^ </p> 相同为0 不同为1
+    public static void yiHuoOperation(){
+        System.out.println("2 binary value:" + Integer.toBinaryString(2));
+        System.out.println("3 binary value:" + Integer.toBinaryString(3));
+        int res = 2 ^ 3;
+        System.out.println("2^3 binary value:" +Integer.toBinaryString(res));
+        System.out.println("2^3 int value:" + res);
     }
     // <p> << </p>
     public static void leftOperation(){
